dd37be13d6d7da9b6622d4d64c1eb676fd3d1da7
Cami676 <abhineetsingh676@gmail.com>
2026-04-28 00:06:07 -0700

    name and desc added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 // CSCI-40 Final Project
-// Author: [Your Name Here]
+// Author: Abhineet SIngh
 
 /* 
- * Description:
+ * Description: 
+ *A C++ program that lets users log and manage their gym workouts.
+ * Each workout entry stores the exercise name, date, sets, reps,
+ * weight lifted, and muscle group, grouped together using a struct.
+ * Entries are held in arrays and saved to a file so data persists
+ * between program runs.
  *
 */
 
